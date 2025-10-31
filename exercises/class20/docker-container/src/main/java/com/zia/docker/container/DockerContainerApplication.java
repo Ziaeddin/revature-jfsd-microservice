32f08d56c8dbe181a68847c0ad4fc8222a44f9bc
zia <zia.najafian@gmail.com>
2025-10-31 11:09:33 -0700

    Class20:dockerization exercise

---
+++ exercises/class20/docker-container/src/main/java/com/zia/docker/container/DockerContainerApplication.java
@@ -0,0 +1,13 @@
+package com.zia.docker.container;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerContainerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerContainerApplication.class, args);
+	}
+
+}
